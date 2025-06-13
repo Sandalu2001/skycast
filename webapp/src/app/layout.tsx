7f71fbd6197d8f28633df24199f37051418041c4
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-06-13 21:27:47 +0530

    Implement theme toggling functionality with light and dark modes

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "../theme";
+import { generateTheme } from "../theme";
 import { usePathname } from "next/navigation";
 import { CssBaseline, Stack } from "@mui/material";
+import { useMemo, useState } from "react";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,14 @@ export default function RootLayout({
 
   // Define routes where the Navbar should not appear
   const noNavbarRoutes = ["/login", "/sign-up"];
+
+  const [mode, setMode] = useState<"dark" | "light">("light");
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
+
+  const theme = useMemo(() => generateTheme(mode), [mode]);
+
   return (
     <html lang="en">
       <ThemeProvider theme={theme}>
--- webapp/src/theme.ts
+++ webapp/src/theme.ts
@@ -1,165 +1,167 @@
 "use client";
 
-import { createTheme } from "@mui/material/styles";
+import { alpha, createTheme, Theme } from "@mui/material/styles";
 import "./app.css";
+import type {} from "@mui/x-data-grid/themeAugmentation";
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#8B80FF",
+export const generateTheme = (mode: "light" | "dark"): Theme => {
+  return createTheme({
+    palette: {
+      primary: {
+        main: "#00A9FF",
+      },
+      secondary: {
+        main: mode == "light" ? "#3D0301" : "",
+      },
+      info: {
+        main: mode == "light" ? "#A294F9" : "",
+      },
+      error: {
+        main: mode == "light" ? "#D35D6E" : "",
+      },
+      success: {
+        main: mode == "light" ? "#659287" : "",
+      },
+      warning: {
+        main: mode == "light" ? "#EFB495" : "",
+      },
+      background: {
+        default: mode == "light" ? "#C4D9FF" : "#453C67", //Main background color
+        paper: mode == "light" ? "#EEF5FF" : "",
+      },
     },
-    secondary: {
-      main: "#3D0301",
+    typography: {
+      fontFamily: ["Karla", "sans-serif"].join(","),
+      fontSize: 10,
+      h1: {
+        fontWeight: 500,
+        fontSize: 65,
+      },
+      h2: {
+        fontWeight: 500,
+      },
+      h3: {
+        fontWeight: 500,
+      },
+      h4: {
+        fontWeight: 500,
+      },
+      h5: {
+        fontWeight: 500,
+      },
+      body1: {
+        fontWeight: 500,
+        fontSize: 14,
+      },
+      body2: {
+        fontWeight: 500,
+        fontSize: 12,
+      },
     },
-
-    info: {
-      main: "#A294F9",
-    },
-    error: {
-      main: "#D35D6E",
-    },
-    success: {
-      main: "#659287",
-    },
-    warning: {
-      main: "#EFB495",
-    },
-    background: {
-      default: "#171717",
-      paper: "#fef9f2",
-    },
-  },
-  typography: {
-    fontFamily: ["Poppins", "sans-serif"].join(","),
-    fontSize: 10,
-    h1: {
-      fontWeight: 500,
-      fontSize: 65,
-    },
-    h2: {
-      fontWeight: 500,
-    },
-    h3: {
-      fontWeight: 500,
-    },
-    h4: {
-      fontWeight: 500,
-    },
-    h5: {
-      fontWeight: 500,
-    },
-    body1: {
-      fontWeight: 500,
-      fontSize: 14,
-    },
-    body2: {
-      fontWeight: 500,
-      fontSize: 12,
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {},
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "& .MuiInputBase-root": {
-            size: "small",
+    components: {
+      MuiButton: {
+        styleOverrides: {},
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiInputBase-root": {
+              size: "small",
+            },
+          },
+        },
+      },
+      MuiDialog: {
+        styleOverrides: {
+          root: {
+            backdropFilter: "blur(5px)",
+          },
+          paper: {
+            width: "90vw",
+            borderRadius: 4,
+            maxHeight: "90vh",
+            "& .MuiDialogTitle-root": {
+              fontWeight: "bold",
+            },
+          },
+        },
+      },
+      MuiDataGrid: {
+        styleOverrides: {
+          root: {
+            fontSize: 13,
+            pb: 0,
+            width: "100%",
+            borderLeft: "none",
+            borderRight: "none",
+            borderBottom: "none",
+            color: alpha("#000", 0.55),
+            borderRadius: 0,
+            "& .MuiDataGrid-cell:focus": {
+              outline: "none",
+            },
+            "& .MuiDataGrid-cell:focus-within": {
+              outline: "none",
+            },
+            ".MuiDataGrid-columnSeparator": {
+              display: "none",
+            },
+            "& .MuiDataGrid-columnHeader:focus": {
+              outline: "none",
+            },
+            "& .MuiDataGrid-columnHeader-within": {
+              outline: "none",
+            },
+            "& .MuiDataGrid-columnHeader:focus-within": {
+              outline: "none",
+            },
+            ".MuiDataGrid-row:focus": {
+              borderRadius: 0,
+            },
+            ".MuiDataGrid-row:focus-within": {
+              borderRadius: 0,
+            },
+            "& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer":
+              {
+                display: "none",
+              },
+            ".MuiDataGrid-columnHeaderTitle": {
+              fontSize: 13,
+              fontWeight: "bold",
+            },
+            "& .MuiDataGrid-columnHeader": {
+              background: "none",
+              fontWeight: "bold",
+              borderRadius: 0,
+            },
+            "& .MuiDataGrid-columnHeaderRow": {
+              background: "none",
+              fontWeight: "bold",
+              borderRadius: 0,
+            },
+            "& .MuiDataGrid-columnHeader--withLeftBorder": {
+              borderRadius: 10,
+            },
+            "& .MuiChartsContainer-root": {
+              padding: 0,
+              margin: 0,
+            },
+            "& .MuiChartsAxis-root": {
+              padding: 0,
+            },
+            "& .MuiChartsAxis-tickContainer": {
+              margin: 0,
+            },
+          },
+          columnHeader: {
+            mb: 2,
+            minHeight: 60,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           },
         },
       },
     },
-    MuiDialog: {
-      styleOverrides: {
-        root: {
-          backdropFilter: "blur(5px)",
-        },
-        paper: {
-          width: "90vw",
-          borderRadius: 4,
-          maxHeight: "90vh",
-          "& .MuiDialogTitle-root": {
-            fontWeight: "bold",
-          },
-        },
-      },
-    },
-    // MuiDataGrid: {
-    //   styleOverrides: {
-    //     root: {
-    //       fontSize: 13,
-    //       pb: 0,
-    //       width: "100%",
-    //       borderLeft: "none",
-    //       borderRight: "none",
-    //       borderBottom: "none",
-    //       color: alpha("#000", 0.55),
-    //       borderRadius: 0,
-    //       "& .MuiDataGrid-cell:focus": {
-    //         outline: "none",
-    //       },
-    //       "& .MuiDataGrid-cell:focus-within": {
-    //         outline: "none",
-    //       },
-    //       ".MuiDataGrid-columnSeparator": {
-    //         display: "none",
-    //       },
-    //       "& .MuiDataGrid-columnHeader:focus": {
-    //         outline: "none",
-    //       },
-    //       "& .MuiDataGrid-columnHeader-within": {
-    //         outline: "none",
-    //       },
-    //       "& .MuiDataGrid-columnHeader:focus-within": {
-    //         outline: "none",
-    //       },
-    //       ".MuiDataGrid-row:focus": {
-    //         borderRadius: 0,
-    //       },
-    //       ".MuiDataGrid-row:focus-within": {
-    //         borderRadius: 0,
-    //       },
-    //       "& .MuiDataGrid-columnHeaderCheckbox .MuiDataGrid-columnHeaderTitleContainer":
-    //         {
-    //           display: "none",
-    //         },
-    //       ".MuiDataGrid-columnHeaderTitle": {
-    //         fontSize: 13,
-    //         fontWeight: "bold",
-    //       },
-    //       "& .MuiDataGrid-columnHeader": {
-    //         background: "none",
-    //         fontWeight: "bold",
-    //         borderRadius: 0,
-    //       },
-    //       "& .MuiDataGrid-columnHeaderRow": {
-    //         background: "none",
-    //         fontWeight: "bold",
-    //         borderRadius: 0,
-    //       },
-    //       "& .MuiDataGrid-columnHeader--withLeftBorder": {
-    //         borderRadius: 10,
-    //       },
-    //       "& .MuiChartsContainer-root": {
-    //         padding: 0, // Reduce chart container padding
-    //         margin: 0, // Reduce chart container margin
-    //       },
-    //       "& .MuiChartsAxis-root": {
-    //         padding: 0, // Reduce axis padding
-    //       },
-    //       "& .MuiChartsAxis-tickContainer": {
-    //         margin: 0, // Reduce tick container margin
-    //       },
-    //     },
-    //     columnHeader: {
-    //       mb: 2,
-    //       minHeight: 60,
-    //       display: "flex",
-    //       alignItems: "center",
-    //       justifyContent: "center",
-    //     },
-    //   },
-    // },
-  },
-});
+  });
+};
