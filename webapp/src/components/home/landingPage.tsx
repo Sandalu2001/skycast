11452e8e226e11cd4adbf8ca972418e3e911bc5c
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-06-14 22:28:23 +0530

    Enhance LandingPage component: add location cards, improve layout, and integrate daily forecast and core data sections

--- webapp/src/components/home/landingPage.tsx
+++ webapp/src/components/home/landingPage.tsx
@@ -3,65 +3,124 @@
 import { alpha, Box, Button, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import DateCard from "../cards/dateCard";
+import LocationCard from "../cards/locationCard";
+import SecondaryLocationCard from "../cards/secondaryLocationCard";
+import { Sunny } from "@mui/icons-material";
+import DailyForcastSection from "./DailyForcast";
+import CoreDataSection from "./coreData";
 
 export default function LandingPage() {
   return (
     <Stack
       sx={{
+        width: "100%",
         height: "100%",
+        overflow: "hidden",
         flexDirection: "row",
       }}
     >
       <Stack
         sx={{
+          p: 8,
+          gap: 4,
           flex: 5,
           background: (theme) => theme.palette.background.paper,
-          flexDirection: "row",
+          height: "100%",
+          alignContent: "space-between",
           justifyContent: "space-between",
-          p: 8,
-          pr: 4,
         }}
       >
-        <DateCard
-          imageURL={"sunny"}
-          day={"Sun"}
-          temperature={"29"}
-          isToday={false}
-        />
-        <DateCard
-          imageURL={"raining"}
-          day={"Mon"}
-          temperature={"29"}
-          isToday={false}
-        />
-        <DateCard
-          imageURL={"slightly-raining"}
-          day={"Tue"}
-          temperature={"29"}
-          isToday={false}
-        />
-        <DateCard imageURL={"sunny"} day={"Wed"} temperature={"29"} isToday />
-        <DateCard
-          imageURL={"thunderstorm"}
-          day={"Thu"}
-          temperature={"29"}
-          isToday={false}
-        />
-        <DateCard
-          imageURL={"windy"}
-          day={"Fri"}
-          temperature={"29"}
-          isToday={false}
-        />
+        <Stack
+          sx={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Stack
+            gap={1}
+            sx={{ width: "100%" }}
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            alignContent={"space-between"}
+          >
+            <Typography
+              variant="h3"
+              color="primary"
+              sx={{
+                fontWeight: 600,
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+              }}
+            >
+              <Sunny fontSize="large" />
+              {`Good morning, Sandalu!`}
+            </Typography>
 
-        <DateCard
-          imageURL={"cloudy"}
-          day={"Sat"}
-          temperature={"29"}
-          isToday={false}
-        />
+            <Stack gap={2}>
+              <Typography variant="h3" color="primary">
+                07.32 AM
+              </Typography>
+              <Typography
+                variant="h5"
+                color="primary.dark"
+                sx={{ fontWeight: 600 }}
+              >
+                Wednesday,14 Jun 2025
+              </Typography>
+            </Stack>
+          </Stack>
+        </Stack>
+        <DailyForcastSection />
+        <CoreDataSection />
       </Stack>
-      <Stack sx={{ height: "100%", flex: 2, p: 8, pl: 4 }}></Stack>
+
+      {/* Location card */}
+      <Stack
+        sx={{
+          height: "100%",
+          p: 5,
+          flex: 2,
+          flexShrink: 0,
+          gap: 3,
+          overflow: "auto",
+        }}
+      >
+        <LocationCard
+          imageURL={"sunny"}
+          day={"Today"}
+          temperature={"29"}
+          location={"New York"}
+          wind={"5 km/h"}
+          humidity={"60%"}
+        />
+        <Stack gap={2}>
+          <SecondaryLocationCard
+            imageURL={"sunny"}
+            day={"Tomorrow"}
+            temperature={"30"}
+            location={"Los Angeles"}
+            wind={"3 km/h"}
+            humidity={"55%"}
+          />
+          <SecondaryLocationCard
+            imageURL={"sunny"}
+            day={"Tomorrow"}
+            temperature={"30"}
+            location={"Los Angeles"}
+            wind={"3 km/h"}
+            humidity={"55%"}
+          />
+          <SecondaryLocationCard
+            imageURL={"sunny"}
+            day={"Tomorrow"}
+            temperature={"30"}
+            location={"Los Angeles"}
+            wind={"3 km/h"}
+            humidity={"55%"}
+          />
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
