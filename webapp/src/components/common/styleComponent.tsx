4fb171904033892acc3decf883dba287c8f4f0b1
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-06-14 22:28:16 +0530

    Add StyledContainer component for enhanced layout styling

---
+++ webapp/src/components/common/styleComponent.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Stack, alpha, useTheme, StackProps } from "@mui/material";
+
+interface StyledContainerProps extends StackProps {
+  children: React.ReactNode;
+}
+
+const StyledContainer = ({ children, sx, ...rest }: StyledContainerProps) => {
+  const theme = useTheme();
+  return (
+    <Stack
+      sx={{
+        borderRadius: 6,
+        background: `linear-gradient(-45deg, ${
+          theme.palette.common.white
+        }, ${alpha(theme.palette.primary.main, 0.1)})`,
+        boxShadow: `
+          inset 0px 5px 15px rgba(0, 0, 0, 0.08),
+          0px 15px 30px rgba(0, 0, 0, 0.15)
+        `,
+        transition: "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
+        "&:hover": {
+          transform: "scale(1.01)",
+          boxShadow: `
+            inset 0px 5px 15px rgba(0, 0, 0, 0.08),
+            0px 25px 50px rgba(0, 0, 0, 0.2)
+          `,
+        },
+        px: 1,
+        py: 3,
+        textAlign: "center",
+        ...sx,
+      }}
+      {...rest}
+    >
+      {children}
+    </Stack>
+  );
+};
+
+export default StyledContainer;
