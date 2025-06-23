a592da35827d54f09c8e15971989cf608bd60c3e
Sandalu De Silva <86417299+Sandalu2001@users.noreply.github.com>
2025-06-23 21:21:02 +0530

    Enhance LabelBottomNavigation component with dynamic styling and improved icon presentation

--- webapp/src/components/common/navBar.tsx
+++ webapp/src/components/common/navBar.tsx
@@ -6,13 +6,41 @@ import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { useTheme } from "@mui/material/styles";
 
 export default function LabelBottomNavigation() {
+  const theme = useTheme();
   const [value, setValue] = React.useState("recents");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
+
+  const getActionStyles = (actionValue: string) => ({
+    "& .MuiBottomNavigationAction-label": {
+      fontWeight: value === actionValue ? "bold" : "normal",
+      color:
+        value === actionValue
+          ? theme.palette.secondary.contrastText
+          : theme.palette.text.primary,
+    },
+    "& .MuiSvgIcon-root": {
+      fontSize: 35,
+      padding: 1,
+
+      color:
+        value === actionValue
+          ? theme.palette.secondary.contrastText
+          : "inherit",
+      transition: "background-color 0.3s, color 0.3s",
+    },
+    padding: 1,
+    borderRadius: 40,
+    backgroundColor:
+      value === actionValue ? theme.palette.secondary.main : "transparent",
+    transition: "background-color 0.3s, color 0.3s",
+    m: 0.4,
+  });
 
   return (
     <BottomNavigation
@@ -28,22 +56,27 @@ export default function LabelBottomNavigation() {
         zIndex: 1000,
         height: 70,
         borderRadius: 40,
+        boxShadow: 3,
+        bgcolor: "background.paper",
       }}
     >
       <BottomNavigationAction
         label="Recents"
         value="recents"
-        icon={<RestoreIcon fontSize="large" />}
+        icon={<RestoreIcon />}
+        sx={getActionStyles("recents")}
       />
       <BottomNavigationAction
         label="Favorites"
         value="favorites"
-        icon={<FavoriteIcon fontSize="large" />}
+        icon={<FavoriteIcon />}
+        sx={getActionStyles("favorites")}
       />
       <BottomNavigationAction
         label="Nearby"
         value="nearby"
-        icon={<LocationOnIcon fontSize="large" />}
+        icon={<LocationOnIcon />}
+        sx={getActionStyles("nearby")}
       />
     </BottomNavigation>
   );
